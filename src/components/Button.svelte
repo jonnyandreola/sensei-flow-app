76de867b2f82bf8c8f7367523cf9a788f2f1a587
Jonny Andreola <jandreola@senseilabs.com>
2021-04-06 13:57:44 -0400

    added markdown editor

---
+++ src/components/Button.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	export let text = 'Button';
+	export let action = () => null;
+	export let disabled = false
+</script>
+
+<button on:click={action} {disabled}>{text}</button>
+
+<style>
+	button {
+		display: inline-block;
+		border: none;
+		padding: 1rem 2rem;
+		margin: 0;
+		text-decoration: none;
+		background: var(--color-link-alt);
+		color: var(--color-link);
+		cursor: pointer;
+		text-align: center;
+		transition: background 250ms ease-in-out, transform 150ms ease;
+		-webkit-appearance: none;
+		-moz-appearance: none;
+	}
+
+	button:hover,
+	button:focus {
+		background: var(--color-link-hover)
+	}
+
+	button:focus {
+		outline: 1px solid var(--color-link);
+		outline-offset: -4px;
+	}
+
+	button:active {
+		transform: scale(0.99);
+	}
+</style>
--- src/pages/New.svelte
+++ src/pages/New.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import dialogPolyfill from 'dialog-polyfill'
-	import { push } from 'svelte-spa-router'
+	import dialogPolyfill from 'dialog-polyfill';
+	import { push } from 'svelte-spa-router';
+	import Button from '../components/Button.svelte';
+	import SimpleMDE from 'simplemde';
 	import { gql } from 'graphql-request';
 	import api from '../helpers/api';
 	import type { QuestionPostRequest, QuestionPostResponse } from '../types/question';
-	import type { GraphQLResponse } from '../types/graphql-response';
+
+	import 'simplemde/dist/simplemde.min.css';
 
 	let isLoading = false;
-	let dialog;
+	let descriptionInput: HTMLTextAreaElement;
+	let descriptionText;
+	let dialog: HTMLDialogElement;
 
 	const newMessageWarningKey = 'senseiflow.ignoreNewQuestionWarning';
 
@@ -17,6 +22,11 @@
 		if (localStorage.getItem(newMessageWarningKey) !== 'true') {
 			dialog.showModal();
 		}
+
+		let simplemde = new SimpleMDE({ element: descriptionInput });
+		simplemde.codemirror.on('change', () => {
+			descriptionText = simplemde.value();
+		})
 	})
 
 	function handleModalClick(e) {
@@ -75,18 +85,22 @@
 </dialog>
 
 <form on:submit="{formSubmit}" autocomplete="off">
-	<div class="form-control">
-		<label for="titleInput">Title</label>
-		<input id="titleInput" type="text" required minlength="20" maxlength="150">
-	</div>
-	<div class="form-control">
-		<label for="descriptionInput">Description</label>
-		<textarea id="descriptionInput" cols="30" rows="10" required></textarea>
+	<div class="input-group">
+		<div class="form-control">
+			<label for="titleInput">Title</label>
+			<input id="titleInput" type="text" required minlength="20" maxlength="150">
+		</div>
+		<div class="form-control">
+			<label for="descriptionInput">Description</label>
+			<div>
+				<textarea bind:this={descriptionInput} bind:value={descriptionText} id="descriptionInput" cols="30" rows="10"></textarea>
+			</div>
+		</div>
 	</div>
 	{#if isLoading}
-		<input type="submit" value="Creating Question..." disabled>
+		<Button text="Creating Question..." disabled></Button>
 	{:else}
-		<input type="submit" value="Create Question">
+		<Button text="Create Question"></Button>
 	{/if}
 </form>
 
@@ -95,13 +109,19 @@
 	dialog {
 		max-width: 500px;
 	}
+
 	form {
 		display: flex;
 		flex-direction: column;
 	}
 
-	input[type=submit] {
+	form :global(button) {
 		align-self: flex-end;
+	}
+
+	.input-group {
+		display: grid;
+		grid-gap: var(--spacing-md);
 	}
 
 	.form-control {
@@ -112,7 +132,7 @@
 		width: 150px;
 	}
 
-	.form-control input, .form-control textarea {
+	.form-control > * {
 		width: 100%;
 	}
 </style>
